203a96bcbb71441668968cf9d8bd2f07cd6f4013
Sainath-Sawale <sainathsawale000@gmail.com>
2025-01-16 11:25:11 +0530

    Add files via upload

---
+++ RBI.java
@@ -0,0 +1,17 @@
+package com.exponent.bankapplication.service;
+
+public interface RBI {
+	public void createAccount();
+	
+	public void showAccountDetails();
+	
+	public void showAccountBalance();
+	
+	public void depositMoney();
+	
+	public void withdrawMoney();
+	
+	public void updateAccountDetails();
+	
+
+}
